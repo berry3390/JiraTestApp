145a6bdb9a9f4f9b2218744440a4b60bf857aaf5
hyejin-QA <berry3390@happytechnology.kr>
2022-06-29 15:07:43 +0900

    [bb-2] check jira automation 수정3

--- app/src/main/java/com/example/jiratestapp/FirstFragment.java
+++ app/src/main/java/com/example/jiratestapp/FirstFragment.java
@@ -29,7 +29,7 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //test 220629 pm0405
+        //test 220629 pm0407
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
